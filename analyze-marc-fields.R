9973cbe73562552c80d05942eaf8f09a88df5c5a
Peter Organisciak <organisciak@gmail.com>
2015-12-06 21:33:34 -0600

    New counting method Instead of count from a download of MARC records, the new Node script now downloads in memory and write a simply "OCLC Field" output to a file, which is subsequently counted up through R.

--- analyze-marc-fields.R
+++ analyze-marc-fields.R
@@ -1,12 +1,14 @@
 require(data.table)
 require(ggplot2)
-data =fread("count-fields.tsv", header=F, colClasses = c("character", "character"))
+data =fread("count-fields-full.tsv", header=F, colClasses = c("character", "character"))
 colnames(data) <- c("Record", "Field")
 
 ref <- fread("marc-ref.tsv", header = T, colClasses = c("character", "character"))
 
 # Count Fields that occur in the most records
 countfields <- data[, list(count=length(unique(Record))), by=list(Field)][order(-count)]
+countfields[['percentage']] <- 100*countfields[['count']]/100000
+
 # Cross-reference with description
 merge(countfields, ref, by="Field", all.x=T)[order(-count)]
 
@@ -19,3 +21,6 @@ data[, list(count=.N), by=list(Field)][order(-count)]
 # See which of the sample records has the most control fields (to use as an example)
 data[, list(Unique.Fields=length(unique(Field))), by=list(Record)][order(-Unique.Fields)]
 
+
+a = merge(countfields, ref, by="Field", all.x=T)[order(-count)]
+write.csv(a, "top-marc-fields.csv", row.names=F)
